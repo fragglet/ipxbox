828a267f9e420b5993b5a0a17aecbb9760b40b2c
Simon Howard <fraggle@soulsphere.org>
2021-01-03 00:51:36 -0500

    Add --enable_ipxpkt command line flag.
    This integrates the code added in the previous few commits into ipxbox,
    so that if the new flag is provided, non-IPX frames will be forwarded
    in ipxpkt protocol format.

--- ipxbox.go
+++ ipxbox.go
@@ -7,6 +7,7 @@ import (
 	"log"
 
 	"github.com/fragglet/ipxbox/bridge"
+	"github.com/fragglet/ipxbox/ipxpkt"
 	"github.com/fragglet/ipxbox/phys"
 	"github.com/fragglet/ipxbox/server"
 	"github.com/fragglet/ipxbox/virtual"
@@ -30,6 +31,7 @@ var (
 	clientTimeout   = flag.Duration("client_timeout", server.DefaultConfig.ClientTimeout, "Time of inactivity before disconnecting clients.")
 	ethernetFraming = flag.String("ethernet_framing", "802.2", `Framing to use when sending Ethernet packets. Valid values are "802.2", "802.3raw", "snap" and "eth-ii".`)
 	allowNetBIOS    = flag.Bool("allow_netbios", false, "If true, allow packets to be forwarded that may contain Windows file sharing (NetBIOS) packets.")
+	enableIpxpkt    = flag.Bool("enable_ipxpkt", false, "If true, route encapsulated packets from the IPXPKT.COM driver to the physical network (requires --enable_tap or --pcap_device)")
 )
 
 func printPackets(v *virtual.Network) {
@@ -52,39 +54,53 @@ func printPackets(v *virtual.Network) {
 	}
 }
 
+func ethernetStream() (phys.DuplexEthernetStream, error) {
+	if *enableTap {
+		return phys.NewTap(water.Config{})
+	} else if *pcapDevice == "" {
+		return nil, nil
+	}
+	// TODO: List
+	handle, err := pcap.OpenLive(*pcapDevice, 1500, true, pcap.BlockForever)
+	if err != nil {
+		return nil, err
+	}
+	// As an optimization we set a filter to only deliver IPX packets
+	// because they're all we care about. However, when ipxpkt routing is
+	// enabled we want all Ethernet frames.
+	if !*enableIpxpkt {
+		if err := handle.SetBPFFilter("ipx"); err != nil {
+			return nil, err
+		}
+	}
+	return handle, nil
+}
+
 func main() {
 	flag.Parse()
-
-	framer, ok := framers[*ethernetFraming]
-	if !ok {
-		log.Fatalf("invalid Ethernet framing %q", *ethernetFraming)
-	}
 
 	var cfg server.Config
 	cfg = *server.DefaultConfig
 	cfg.ClientTimeout = *clientTimeout
 	v := virtual.New()
 	v.BlockNetBIOS = !*allowNetBIOS
-	if *enableTap {
-		stream, err := phys.NewTap(water.Config{})
-		if err != nil {
-			log.Fatalf("failed to start tap: %v", err)
+
+	stream, err := ethernetStream()
+	if err != nil {
+		log.Fatalf("failed to set up physical network: %v", err)
+	} else if stream != nil {
+		framer, ok := framers[*ethernetFraming]
+		if !ok {
+			log.Fatalf("unknown Ethernet framing %q", *ethernetFraming)
 		}
+
 		p := phys.NewPhys(stream, framer)
 		tap := v.Tap()
 		go bridge.Run(tap, tap, p, p)
-	} else if *pcapDevice != "" {
-		// TODO: List
-		handle, err := pcap.OpenLive(*pcapDevice, 1500, true, pcap.BlockForever)
-		if err != nil {
-			log.Fatalf("failed to open pcap: %v", err)
+		if *enableIpxpkt {
+			r := ipxpkt.NewRouter(v.NewNode())
+			go phys.CopyFrames(r, p.NonIPX())
 		}
-		if err := handle.SetBPFFilter("ipx"); err != nil {
-			log.Fatalf("failed to set filter: %v", err)
-		}
-		p := phys.NewPhys(handle, framer)
-		tap := v.Tap()
-		go bridge.Run(tap, tap, p, p)
 	}
 	if *dumpPackets {
 		go printPackets(v)
