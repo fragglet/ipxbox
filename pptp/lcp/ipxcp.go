85a7380207944c399bcab34cffcef2a793ac3fea
Simon Howard <fraggle@soulsphere.org>
2021-07-30 13:41:53 -0400

    Add IPXCP dialect of LCP.

---
+++ pptp/lcp/ipxcp.go
@@ -0,0 +1,41 @@
+package lcp
+
+import (
+	"github.com/google/gopacket"
+)
+
+var LayerTypeIPXCP = gopacket.RegisterLayerType(1819, gopacket.LayerTypeMetadata{
+	Name:    "IPXCP",
+	Decoder: gopacket.DecodeFunc(decodeIPXCP),
+})
+
+// TODO: Implement SerializeTo and make this SerializableLayer.
+var _ = gopacket.Layer(&LCP{})
+
+var (
+	OptionIPXNetwork               = OptionType{ipxcpDialect, 1}
+	OptionIPXNode                  = OptionType{ipxcpDialect, 2}
+	OptionIPXCompressionProtocol   = OptionType{ipxcpDialect, 3}
+	OptionIPXRoutingProtocol       = OptionType{ipxcpDialect, 4}
+	OptionIPXRouterName            = OptionType{ipxcpDialect, 5}
+	OptionIPXConfigurationComplete = OptionType{ipxcpDialect, 6}
+)
+
+// IPXCP is a gopacket layer for the PPP IPX Control Protocol.
+type IPXCP struct {
+	baseLCP
+}
+
+func (l *IPXCP) LayerType() gopacket.LayerType {
+	return LayerTypeIPXCP
+}
+
+func decodeIPXCP(data []byte, p gopacket.PacketBuilder) error {
+	ipxcp := &IPXCP{}
+	ipxcp.baseLCP.dialect = ipxcpDialect
+	if err := ipxcp.UnmarshalBinary(data); err != nil {
+		return err
+	}
+	p.AddLayer(ipxcp)
+	return nil
+}
