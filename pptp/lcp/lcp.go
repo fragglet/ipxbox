d967f484e8d92332ba1a28c930cdc463bbd6e074
Simon Howard <fraggle@soulsphere.org>
2021-07-30 13:20:57 -0400

    Factor out main LCP unmarshaling to base type.
    The LCP wire format is reused for other protocols such as IPXCP.
    Therefore factor the main code out to a base type so that it can be
    reused.

--- pptp/lcp/lcp.go
+++ pptp/lcp/lcp.go
@@ -105,12 +105,48 @@ func (d *EchoData) UnmarshalBinary(data []byte) error {
 	return nil
 }
 
-// LCP is a gopacket layer for the Link Control Protocol.
-type LCP struct {
+// baseLCP represents the basic LCP layer used by LCP proper
+// and other dialects that reuse the same format.
+type baseLCP struct {
 	layers.BaseLayer
 	Type       MessageType
 	Identifier uint8
 	Data       PerTypeData
+}
+
+func (l *baseLCP) UnmarshalBinary(data []byte) error {
+	if len(data) < 4 {
+		return MessageTooShort
+	}
+	l.Type = MessageType(data[0])
+	l.Identifier = data[1]
+	lenField := binary.BigEndian.Uint16(data[2:4])
+	if int(lenField) > len(data) {
+		return MessageTooShort
+	}
+
+	switch l.Type {
+	case ConfigureRequest, ConfigureAck, ConfigureNak, ConfigureReject:
+		l.Data = &ConfigureData{}
+	case TerminateRequest, TerminateAck:
+		l.Data = &TerminateData{}
+	case EchoRequest, EchoReply:
+		l.Data = &EchoData{}
+		// TODO: Other message types.
+	}
+	if l.Data != nil {
+		if err := l.Data.UnmarshalBinary(data[4:]); err != nil {
+			return err
+		}
+	}
+	l.Contents = data
+	l.Payload = nil
+	return nil
+}
+
+// LCP is a gopacket layer for the Link Control Protocol.
+type LCP struct {
+	baseLCP
 }
 
 func (l *LCP) LayerType() gopacket.LayerType {
@@ -119,32 +155,9 @@ func (l *LCP) LayerType() gopacket.LayerType {
 
 func decodeLCP(data []byte, p gopacket.PacketBuilder) error {
 	lcp := &LCP{}
-	if len(data) < 4 {
-		return MessageTooShort
+	if err := lcp.UnmarshalBinary(data); err != nil {
+		return err
 	}
-	lcp.Type = MessageType(data[0])
-	lcp.Identifier = data[1]
-	lenField := binary.BigEndian.Uint16(data[2:4])
-	if int(lenField) > len(data) {
-		return MessageTooShort
-	}
-
-	switch lcp.Type {
-	case ConfigureRequest, ConfigureAck, ConfigureNak, ConfigureReject:
-		lcp.Data = &ConfigureData{}
-	case TerminateRequest, TerminateAck:
-		lcp.Data = &TerminateData{}
-	case EchoRequest, EchoReply:
-		lcp.Data = &EchoData{}
-		// TODO: Other message types.
-	}
-	if lcp.Data != nil {
-		if err := lcp.Data.UnmarshalBinary(data[4:]); err != nil {
-			return err
-		}
-	}
-	lcp.Contents = data
-	lcp.Payload = nil
 	p.AddLayer(lcp)
 	return nil
 }
