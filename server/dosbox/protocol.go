7eb3356061190f6933a58a7949b4be8ca8bc9a41
Simon Howard <fraggle@soulsphere.org>
2022-03-06 00:17:24 -0500

    Move DosboxProtocol logic to submodule.

---
+++ server/dosbox/protocol.go
@@ -0,0 +1,128 @@
+// Package server implements the server side of the DOSBox IPX protocol.
+package server
+
+import (
+	"context"
+	"sync"
+	"time"
+
+	"github.com/fragglet/ipxbox/ipx"
+)
+
+var (
+	_ = (ipx.ReadWriteCloser)(&Protocol{})
+
+	// Server-initiated pings come from this address.
+	addrPingReply = [6]byte{0x02, 0xff, 0xff, 0xff, 0x00, 0x00}
+)
+
+// Protocol implements the dosbox protocol as a wrapper around an
+// inner ReadWriteCloser that is used to send and receive IPX frames.
+type Protocol struct {
+	inner        ipx.ReadWriteCloser
+	nodeAddr     *ipx.Addr
+	mu           sync.Mutex
+	lastRecvTime time.Time
+}
+
+func MakeProtocol(inner ipx.ReadWriteCloser, nodeAddr *ipx.Addr) *Protocol {
+	return &Protocol{
+		inner:    inner,
+		nodeAddr: nodeAddr,
+	}
+}
+
+func (p *Protocol) ReadPacket(ctx context.Context) (*ipx.Packet, error) {
+	for {
+		packet, err := p.inner.ReadPacket(ctx)
+		if err != nil {
+			return nil, err
+		}
+		p.mu.Lock()
+		p.lastRecvTime = time.Now()
+		p.mu.Unlock()
+		if packet.Header.IsRegistrationPacket() {
+			p.SendRegistrationReply()
+			continue
+		}
+		return packet, nil
+	}
+}
+
+func (p *Protocol) WritePacket(packet *ipx.Packet) error {
+	return p.inner.WritePacket(packet)
+}
+
+func (p *Protocol) Close() error {
+	return p.inner.Close()
+}
+
+// SendRegistrationReply sends a response to the client when a registration
+// packet is received. This usually happens only once on first connect,
+// unless the reply is lost in transit.
+func (p *Protocol) SendRegistrationReply() {
+	p.inner.WritePacket(&ipx.Packet{
+		Header: ipx.Header{
+			Checksum:     0xffff,
+			Length:       30,
+			TransControl: 0,
+			Dest: ipx.HeaderAddr{
+				Network: [4]byte{0, 0, 0, 0},
+				Addr:    *p.nodeAddr,
+				Socket:  2,
+			},
+			Src: ipx.HeaderAddr{
+				Network: [4]byte{0, 0, 0, 1},
+				Addr:    ipx.AddrBroadcast,
+				Socket:  2,
+			},
+		},
+	})
+}
+
+// sendPing transmits a ping packet to the given client. The DOSbox IPX client
+// code recognizes broadcast packets sent to socket=2 and will send a reply to
+// the source address that we provide.
+func (p *Protocol) sendPing() {
+	p.inner.WritePacket(&ipx.Packet{
+		Header: ipx.Header{
+			Dest: ipx.HeaderAddr{
+				Addr:   ipx.AddrBroadcast,
+				Socket: 2,
+			},
+			// We send pings from an imaginary "ping reply" address
+			// because if we used ipx.AddrNull the reply would be
+			// indistinguishable from a registration packet.
+			Src: ipx.HeaderAddr{
+				Addr:   addrPingReply,
+				Socket: 0,
+			},
+		},
+	})
+}
+
+// SendKeepalives runs as a background goroutine while a client is connected,
+// sending keepalive pings to keep the connection alive.
+func (p *Protocol) SendKeepalives(ctx context.Context, checkPeriod time.Duration) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-time.After(checkPeriod):
+		}
+		now := time.Now()
+		p.mu.Lock()
+		lastRecvTime := p.lastRecvTime
+		p.mu.Unlock()
+		// Nothing sent in a while? Send a keepalive. This is
+		// important because some games use a client/server
+		// arrangement where the server does not broadcast
+		// anything but listens for broadcasts from clients. An
+		// example is Warcraft 2. If there is no activity
+		// between the client and server in a long time, some
+		// NAT gateways or firewalls can drop the association.
+		if now.After(lastRecvTime.Add(checkPeriod)) {
+			p.sendPing()
+		}
+	}
+}
--- server/server.go
+++ server/server.go
@@ -14,11 +14,11 @@ import (
 	"github.com/fragglet/ipxbox/network"
 	"github.com/fragglet/ipxbox/network/pipe"
 	"github.com/fragglet/ipxbox/network/stats"
+	dosbox "github.com/fragglet/ipxbox/server/dosbox"
 )
 
 var (
 	_ = (ipx.ReadWriteCloser)(&client{})
-	_ = (ipx.ReadWriteCloser)(&DosboxProtocol{})
 )
 
 // Config contains configuration parameters for an IPX server.
@@ -87,9 +87,6 @@ var (
 		KeepaliveTime: 5 * time.Second,
 	}
 
-	// Server-initiated pings come from this address.
-	addrPingReply = [6]byte{0x02, 0xff, 0xff, 0xff, 0x00, 0x00}
-
 	_ = (io.Closer)(&Server{})
 )
 
@@ -113,111 +110,8 @@ func New(addr string, n network.Network, c *Config) (*Server, error) {
 	return s, nil
 }
 
-// DosboxProtocol implements the dosbox protocol as a wrapper around an
-// inner ReadWriteCloser that is used to send and receive IPX frames.
-type DosboxProtocol struct {
-	inner        ipx.ReadWriteCloser
-	nodeAddr     *ipx.Addr
-	mu           sync.Mutex
-	lastRecvTime time.Time
-}
-
-func (p *DosboxProtocol) ReadPacket(ctx context.Context) (*ipx.Packet, error) {
-	for {
-		packet, err := p.inner.ReadPacket(ctx)
-		if err != nil {
-			return nil, err
-		}
-		p.mu.Lock()
-		p.lastRecvTime = time.Now()
-		p.mu.Unlock()
-		if packet.Header.IsRegistrationPacket() {
-			p.sendRegistrationReply()
-			continue
-		}
-		return packet, nil
-	}
-}
-
-func (p *DosboxProtocol) WritePacket(packet *ipx.Packet) error {
-	return p.inner.WritePacket(packet)
-}
-
-func (p *DosboxProtocol) Close() error {
-	return p.inner.Close()
-}
-
-// sendRegistrationReply sends a response to the client when a registration
-// packet is received. This usually happens only once on first connect,
-// unless the reply is lost in transit.
-func (p *DosboxProtocol) sendRegistrationReply() {
-	p.inner.WritePacket(&ipx.Packet{
-		Header: ipx.Header{
-			Checksum:     0xffff,
-			Length:       30,
-			TransControl: 0,
-			Dest: ipx.HeaderAddr{
-				Network: [4]byte{0, 0, 0, 0},
-				Addr:    *p.nodeAddr,
-				Socket:  2,
-			},
-			Src: ipx.HeaderAddr{
-				Network: [4]byte{0, 0, 0, 1},
-				Addr:    ipx.AddrBroadcast,
-				Socket:  2,
-			},
-		},
-	})
-}
-
-// sendPing transmits a ping packet to the given client. The DOSbox IPX client
-// code recognizes broadcast packets sent to socket=2 and will send a reply to
-// the source address that we provide.
-func (p *DosboxProtocol) sendPing() {
-	p.inner.WritePacket(&ipx.Packet{
-		Header: ipx.Header{
-			Dest: ipx.HeaderAddr{
-				Addr:   ipx.AddrBroadcast,
-				Socket: 2,
-			},
-			// We send pings from an imaginary "ping reply" address
-			// because if we used ipx.AddrNull the reply would be
-			// indistinguishable from a registration packet.
-			Src: ipx.HeaderAddr{
-				Addr:   addrPingReply,
-				Socket: 0,
-			},
-		},
-	})
-}
-
-// sendKeepalives runs as a background goroutine while a client is connected,
-// sending keepalive pings to keep the connection alive.
-func (p *DosboxProtocol) sendKeepalives(ctx context.Context, checkPeriod time.Duration) {
-	for {
-		select {
-		case <-ctx.Done():
-			return
-		case <-time.After(checkPeriod):
-		}
-		now := time.Now()
-		p.mu.Lock()
-		lastRecvTime := p.lastRecvTime
-		p.mu.Unlock()
-		// Nothing sent in a while? Send a keepalive. This is
-		// important because some games use a client/server
-		// arrangement where the server does not broadcast
-		// anything but listens for broadcasts from clients. An
-		// example is Warcraft 2. If there is no activity
-		// between the client and server in a long time, some
-		// NAT gateways or firewalls can drop the association.
-		if now.After(lastRecvTime.Add(checkPeriod)) {
-			p.sendPing()
-		}
-	}
-}
-
 // startClient is invoked as a new goroutine when a new client connects.
+// TODO: Move to dosbox protocol package
 func startClient(ctx context.Context, c *client) error {
 	packet, err := c.ReadPacket(ctx)
 	if err != nil {
@@ -239,14 +133,10 @@ func startClient(ctx context.Context, c *client) error {
 
 	c.s.log("%s: new connection, assigned IPX address %s",
 		c.addr.String(), network.NodeAddress(node))
-	p := &DosboxProtocol{
-		inner:        c,
-		nodeAddr:     &nodeAddr,
-		lastRecvTime: time.Now(),
-	}
-	p.sendRegistrationReply()
+	p := dosbox.MakeProtocol(c, &nodeAddr)
+	p.SendRegistrationReply()
 
-	go p.sendKeepalives(ctx, c.s.config.KeepaliveTime)
+	go p.SendKeepalives(ctx, c.s.config.KeepaliveTime)
 
 	return ipx.DuplexCopyPackets(ctx, p, node)
 }
