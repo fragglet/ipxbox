fcd2e505cfe046e54254f633aaf591cdc37b199d
Simon Howard <fraggle@soulsphere.org>
2025-03-04 13:53:52 -0500

    Add a new flag type for ethernet framing
    This validates the ethernet framing at flag parsing time rather than
    later on.

--- phys/flags.go
+++ phys/flags.go
@@ -20,13 +20,13 @@ var (
 
 type Flags struct {
 	Bridge          *Spec
-	EthernetFraming *string
+	EthernetFraming *Framer
 }
 
 func RegisterFlags() *Flags {
 	f := &Flags{}
 	f.Bridge = SpecFlag("bridge", "", `Bridge to physical network. Valid values are: "tap:" or "pcap:{device name}"`)
-	f.EthernetFraming = flag.String("ethernet_framing", "auto", `Framing to use when sending Ethernet packets. Valid values are "auto", "802.2", "802.3raw", "snap" and "eth-ii".`)
+	f.EthernetFraming = FramingTypeFlag("ethernet_framing", `Framing to use when sending Ethernet packets. Valid values are "auto", "802.2", "802.3raw", "snap" and "eth-ii".`)
 	return f
 }
 
@@ -34,31 +34,12 @@ func (f *Flags) EthernetStream(captureNonIPX bool) (DuplexEthernetStream, error)
 	return f.Bridge.Type(f.Bridge.Arg, captureNonIPX)
 }
 
-func (f *Flags) makeFramer() (Framer, error) {
-	framerName := *f.EthernetFraming
-	if framerName == "auto" {
-		return &automaticFramer{
-			fallback: Framer802_2,
-		}, nil
-	}
-	for _, framer := range allFramers {
-		if framerName == framer.Name() {
-			return framer, nil
-		}
-	}
-	return nil, fmt.Errorf("unknown Ethernet framing %q", framerName)
-}
-
 func (f *Flags) MakePhys(captureNonIPX bool) (*Phys, error) {
 	stream, err := f.EthernetStream(captureNonIPX)
 	if err != nil {
 		return nil, err
 	} else if stream != nil {
-		framer, err := f.makeFramer()
-		if err != nil {
-			return nil, err
-		}
-		return NewPhys(stream, framer), nil
+		return NewPhys(stream, *f.EthernetFraming), nil
 	}
 	// Physical capture not enabled.
 	return nil, nil
@@ -103,4 +84,28 @@ func SpecFlag(name, defaultValue, usage string) *Spec {
 	}
 	flag.Func(name, usage, setValue)
 	return result
+}
+
+func framerNames() string {
+	result := []string{}
+	for _, f := range allFramers {
+		result = append(result, fmt.Sprintf("%q", f.Name()))
+	}
+	return strings.Join(result, ", ")
+}
+
+func FramingTypeFlag(name, usage string) *Framer {
+	var result Framer
+	result = &automaticFramer{fallback: Framer802_2}
+	setValue := func(s string) error {
+		for _, framer := range allFramers {
+			if s == framer.Name() {
+				result = framer
+				return nil
+			}
+		}
+		return fmt.Errorf("unknown Ethernet framing %q: valid values are: %v.", s, framerNames())
+	}
+	flag.Func(name, usage, setValue)
+	return &result
 }
