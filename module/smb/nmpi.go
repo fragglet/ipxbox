7e070718ad2e6e821802f217f776b8cd91fd47d0
Simon Howard <fraggle@soulsphere.org>
2026-05-06 20:09:53 -0400

    Initial implementation of NMPI announcer
    In order to be present on the network as a NetBIOS node, we need to
    register our name and respond to queries for it. This implements that.

--- module/smb/nmpi.go
+++ module/smb/nmpi.go
@@ -3,8 +3,12 @@ package smb
 import (
 	"encoding"
 	"encoding/binary"
+	"errors"
 	"fmt"
 	"strings"
+	"time"
+
+	"github.com/fragglet/ipxbox/ipx"
 )
 
 const (
@@ -17,12 +21,22 @@ const (
 	NmpiTypeWorkgroup = 2
 	NmpiTypeBrowser   = 3
 
+	nmpiSocket         = 0x551
 	nmpiMinFrameLength = 68
+
+	// From smbpub.txt:
+	// When the server starts, it sends broadcasts a name claim
+	// (inm_type == INAME_CLAIM) packet five (5) times at 500
+	// millisecond intervals.
+	registrationAttempts = 5
+	registrationSpacing  = 500 * time.Millisecond
 )
 
 var (
 	_ = (encoding.BinaryMarshaler)(&NmpiFrame{})
 	_ = (encoding.BinaryUnmarshaler)(&NmpiFrame{})
+
+	ErrRegistrationConflict = errors.New("name conflict during registration")
 )
 
 // NmpiFrame represents an NMPI frame used for NetBIOS name management over
@@ -71,4 +85,136 @@ func (f *NmpiFrame) MarshalBinary() ([]byte, error) {
 	putNetbiosName(result[36:52], f.name, f.service)
 	putNetbiosName(result[52:68], f.sourceName, f.sourceService)
 	return result, nil
+}
+
+type announcerState int
+
+const (
+	stateRegistering = announcerState(iota)
+	stateConflict
+	stateRegistered
+)
+
+// NmpiAnnouncer registers a NetBIOS name on the network and responds
+// to queries for that name.
+type NmpiAnnouncer struct {
+	state        announcerState
+	name         string
+	service      uint8
+	addr         ipx.HeaderAddr
+	writer       ipx.Writer
+	nameConflict bool
+}
+
+func NewNmpiAnnouncer(name string, service uint8, addr ipx.HeaderAddr, writer ipx.Writer) *NmpiAnnouncer {
+	return &NmpiAnnouncer{
+		name:    name,
+		service: service,
+		addr:    addr,
+		writer:  writer,
+	}
+}
+
+// handleQuery is called when an NMPI "query" or "claim" frame is received.
+func (a *NmpiAnnouncer) handleQuery(pkt *ipx.Packet, f *NmpiFrame) error {
+
+	// Only reply after registration has succeeded.
+	if a.state != stateRegistered {
+		return nil
+	}
+
+	reply := *f
+	reply.operation = NmpiOpFound
+	reply.name = f.sourceName
+	reply.service = f.sourceService
+	reply.sourceName = a.name
+	reply.sourceService = a.service
+
+	payload, _ := reply.MarshalBinary()
+
+	return a.writer.WritePacket(&ipx.Packet{
+		Header: ipx.Header{
+			Dest: pkt.Header.Src,
+			Src:  a.addr,
+		},
+		Payload: payload,
+	})
+}
+
+// handleClaim is called when an NMPI "found" frame is received.
+func (a *NmpiAnnouncer) handleFound(pkt *ipx.Packet, f *NmpiFrame) error {
+	if a.state == stateRegistering {
+		a.state = stateConflict
+	}
+
+	return nil
+}
+
+// WritePacket processes a packet received at the NMPI socket number.
+func (a *NmpiAnnouncer) WritePacket(pkt *ipx.Packet) error {
+	var f NmpiFrame
+	if err := f.UnmarshalBinary(pkt.Payload); err != nil {
+		return err
+	}
+	// We only care about our name.
+	if f.name != a.name || f.service != a.service {
+		return nil
+	}
+	// Loopback packet?
+	if pkt.Header.Src == a.addr {
+		return nil
+	}
+	switch f.operation {
+	case NmpiOpClaim, NmpiOpQuery:
+		return a.handleQuery(pkt, &f)
+	case NmpiOpFound:
+		return a.handleFound(pkt, &f)
+	}
+	return nil
+}
+
+// Register sends several NMPI registration packets to attempt to the network
+// to see if anyone else is using the same name. It blocks until this process
+// completes. If successful, the announcer will start responding to name
+// queries.
+func (a *NmpiAnnouncer) Register() error {
+	a.state = stateRegistering
+
+	msg := &NmpiFrame{
+		operation:     NmpiOpClaim,
+		nameType:      NmpiTypeMachine,
+		name:          a.name,
+		service:       a.service,
+		sourceName:    a.name,
+		sourceService: a.service,
+	}
+	payload, _ := msg.MarshalBinary()
+
+	for i := 0; i < registrationAttempts; i++ {
+		err := a.writer.WritePacket(&ipx.Packet{
+			Header: ipx.Header{
+				Dest: ipx.HeaderAddr{
+					Addr:   ipx.AddrBroadcast,
+					Socket: nmpiSocket,
+				},
+				Src: a.addr,
+			},
+			Payload: payload,
+		})
+		if err != nil {
+			return err
+		}
+
+		// Wait 500ms before sending another registration attempt.
+		// After the pause, we check to see if there was a conflict.
+		time.Sleep(registrationSpacing)
+		if a.state == stateConflict {
+			return ErrRegistrationConflict
+		}
+	}
+
+	// Registration successful
+	a.state = stateRegistered
+
+	return nil
 }
--- module/smb/nmpi_test.go
+++ module/smb/nmpi_test.go
@@ -1,9 +1,14 @@
 package smb
 
 import (
-	"fmt"
+	"context"
 	"reflect"
 	"testing"
+
+	"github.com/fragglet/ipxbox/ipx"
+	"github.com/fragglet/ipxbox/network"
+	"github.com/fragglet/ipxbox/network/addressable"
+	"github.com/fragglet/ipxbox/network/ipxswitch"
 )
 
 func TestFrameMarshalUnmarshal(t *testing.T) {
@@ -72,5 +77,104 @@ func TestFrameMarshalUnmarshal(t *testing.T) {
 					test.data, gotData)
 			}
 		})
+	}
+}
+
+func TestAnnouncer(t *testing.T) {
+	net := addressable.Wrap(ipxswitch.New())
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// First announcer should register successfully.
+	{
+		node, err := net.NewNode()
+		if err != nil {
+			t.Errorf("error creating node: %v", err)
+		}
+		testAddr := ipx.HeaderAddr{
+			Addr:   network.NodeAddress(node),
+			Socket: 0x553,
+		}
+		a := NewNmpiAnnouncer("FOOBAR", 0, testAddr, node)
+		go ipx.CopyPackets(ctx, node, a)
+		if err = a.Register(); err != nil {
+			t.Errorf("failed to register primary announcer: %v", err)
+		}
+	}
+
+	// If we send a query to the network, we should get a reply.
+	{
+		node, err := net.NewNode()
+		if err != nil {
+			t.Errorf("error creating node: %v", err)
+		}
+		queryAddr := ipx.HeaderAddr{
+			Addr:   network.NodeAddress(node),
+			Socket: 0x553,
+		}
+		msg := &NmpiFrame{
+			operation:     NmpiOpQuery,
+			nameType:      NmpiTypeMachine,
+			name:          "FOOBAR",
+			service:       0,
+			sourceName:    "BAZ",
+			sourceService: 0,
+		}
+		payload, _ := msg.MarshalBinary()
+		err = node.WritePacket(&ipx.Packet{
+			Header: ipx.Header{
+				Dest: ipx.HeaderAddr{
+					Addr:   ipx.AddrBroadcast,
+					Socket: nmpiSocket,
+				},
+				Src: queryAddr,
+			},
+			Payload: payload,
+		})
+		if err != nil {
+			t.Errorf("error writing packet: %v", err)
+		}
+		// Read the reply the announcer sends back:
+		pkt, err := node.ReadPacket(ctx)
+		if err != nil {
+			t.Errorf("error reading packet: %v", err)
+		}
+		if pkt.Header.Dest != queryAddr {
+			t.Errorf("reply to wrong address: want %v, got %v", queryAddr, pkt.Header.Dest)
+		}
+		var got NmpiFrame
+		err = got.UnmarshalBinary(pkt.Payload)
+		if err != nil {
+			t.Errorf("error unmarshaling payload: %v", err)
+		}
+		want := &NmpiFrame{
+			operation:     NmpiOpFound,
+			nameType:      NmpiTypeMachine,
+			name:          "BAZ",
+			service:       0,
+			sourceName:    "FOOBAR",
+			sourceService: 0,
+		}
+		if !reflect.DeepEqual(want, &got) {
+			t.Errorf("wrong query response:\nwant %#v\ngot %#v", want, &got)
+		}
+	}
+
+	// We add a second announcer to the network; when it tries to
+	// register it should fail.
+	{
+		node, err := net.NewNode()
+		if err != nil {
+			t.Errorf("error creating node: %v", err)
+		}
+		testAddr := ipx.HeaderAddr{
+			Addr:   network.NodeAddress(node),
+			Socket: 0x553,
+		}
+		a := NewNmpiAnnouncer("FOOBAR", 0, testAddr, node)
+		go ipx.CopyPackets(ctx, node, a)
+		if err = a.Register(); err == nil {
+			t.Errorf("expected secondary announcer to fail, got success")
+		}
 	}
 }
